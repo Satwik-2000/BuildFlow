6fdb6d54ff51743155d3203ba27c6ae3a49dcfe2
Satwik Ghosh <57412553+Satwik-2000@users.noreply.github.com>
2026-02-05 19:15:46 +0000

    change

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
   },
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
+  ...(process.env.DATABASE_URL && {
+    datasource: {
+      url: env("DATABASE_URL"),
+    },
+  }),
 });
